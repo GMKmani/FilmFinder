f7284d3ab78518fbf96baf1b4366569558cb0bfd
Mani <manikumar81796@gmail.com>
2024-09-17 15:06:19 +0530

    add docker

--- FilmFinderApi/obj/Debug/net8.0/FilmFinderApi.AssemblyInfo.cs
+++ FilmFinderApi/obj/Debug/net8.0/FilmFinderApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FilmFinderApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+deaa9d66d3c599aff7c316f8d5fe4fd841a5effa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d0377947f30cd3454532e1d9348611f7e21298b")]
 [assembly: System.Reflection.AssemblyProductAttribute("FilmFinderApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FilmFinderApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
